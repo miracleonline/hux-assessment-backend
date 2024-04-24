5e7f68da63b50a3b0b7357ed3228ff1261508316
Miracle Nwabuisi <110323526+miracleonline@users.noreply.github.com>
2024-04-24 00:44:24 -1200

    Create ContactController.php

---
+++ controllers/ContactController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class ContactController {
+    
+    public function create(Request $request, Response $response, $args) {
+        // Implementation of creating a new contact
+    }
+
+    public function getAll(Request $request, Response $response, $args) {
+        // Implementation of retrieving all contacts
+    }
+
+    public function getOne(Request $request, Response $response, $args) {
+       
