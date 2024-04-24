2b8877f1a4a21482fb666434d414b6b76af1c3b8
Miracle Nwabuisi <110323526+miracleonline@users.noreply.github.com>
2024-04-24 00:42:06 -1200

    Create authController.php

---
+++ controllers/authController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class AuthController {
+    
+    public function register(Request $request, Response $response, $args) {
+        // Implementation of user registration
+    }
+
+    public function login(Request $request, Response $response, $args) {
+        // Implementation of user login
+    }
+}
